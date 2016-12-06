74cf5f124f230f935e499da920af9c0cb431b67c
salah ahmed <salah.ahmed.hslc@gmail.com>
2016-12-05 21:04:18 -0500

    add tests

---
+++ tests/test_crud.py
@@ -0,0 +1,38 @@
+import pytest
+import requests
+
+
+port = 8000
+url = 'http://localhost:%d/%s' % port
+
+
+@pytest.fixture
+def example()
+    return 1
+
+
+def test_add(example):
+    obj = {}
+    post_url = 'add_item'
+    obj_id = requests.post(url % post_url, data=obj)
+    assert obj_id
+    obj_id = int(obj_id)
+    dbobj = session.query(Restaurant).get(obj_id)
+    assert dbobj
+
+
+def test_edit(example):
+    old_obj = session.query(Restaurant).first()
+    post_url = 'edit_item/%d' % old_obj.id
+    new_name='test name'
+    obj = {name=new_name, **old_obj}
+    requests.post(url % post_url, data=obj)
+    assert obj_id
+    new_obj = session.query(Restaurant).get(old_obj.id)
+    assert new_obj
+    assert new_obj.name == new_name
+    assert old_obj.name != new_name
+
+
+def test_delete(example):
+    assert 1 > 0
